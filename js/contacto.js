596f675067db40dad6cf684f8cc7e960ea85e92d
CarlosCala <carlosdev52@gmail.com>
2022-12-21 08:49:14 -0300

    pagina contacto

---
+++ js/contacto.js
@@ -0,0 +1,26 @@
+
+let nombre = document.getElementById("nombreContacto")
+let email = document.getElementById("emailContacto")
+let consulta = document.getElementById("consultaContacto")
+
+let btnEnviar = document.getElementById("btnEnviar")
+
+btnEnviar.addEventListener("click", enviarMail)
+
+let templateParams = {
+    from_name: `${nombre.value}`,
+    Administrador_name: 'Administrador',
+    destinatario: "carlosdev52@gmail.com",
+    message:  `${consulta.value}` ,
+};
+
+function enviarMail(e) {
+    e.preventDefault();
+    console.log("probando funcion");
+    emailjs.send('service_lfzlfu5', 'template_akjtwwz', templateParams)
+        .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function (error) {
+            console.log('FAILED...', error);
+        });
+}
